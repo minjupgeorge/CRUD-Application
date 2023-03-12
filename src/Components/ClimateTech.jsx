166773a772156fc4f2c7bafb3328ade3f21902d8
minjupgeorge <minjupgeorge>
2023-03-12 07:41:40 +0100

    image added

--- src/Components/ClimateTech.jsx
+++ src/Components/ClimateTech.jsx
@@ -1,6 +1,7 @@
 import { Box, Typography, styled } from '@mui/material';
 
 
+
 const Header = styled(Box)`
     margin: 50px;
     & > div {
@@ -13,12 +14,23 @@ const Header = styled(Box)`
 const ClimateTech = () => {
     
     return (
+        
         <Header>
             <Typography variant="h3">Dream Job in Climatic Tech</Typography>
             <Typography variant="h6">Climate Tech refers to an array of technology solutions designed to address climate change and its environmental effects. 
-            This can be done by reducing GHG emissions or adapting our systems to environmental changes.</Typography>
-            <Box style={{display: 'flex'}}>
-            </Box>
+            This can be done by reducing GHG emissions or adapting our systems to environmental changes.          
+            </Typography>
+              <Box
+      sx={{
+        alignItems: "stretch",
+        backgroundImage: `url('${process.env.PUBLIC_URL}/assets/carbon.png')`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        height: "auto",
+        maxWidth: "100%",
+      }}
+    ></Box>
         </Header>
     )
 }
